5a9b74038dac24497313a794013e32780f3c2039
afifdiy <afifdiy@gmail.com>
2022-10-05 12:53:55 +0700

    revert urgent check time

--- warehouseOptimization.py
+++ warehouseOptimization.py
@@ -27,7 +27,7 @@ total_item_picked = list()
 # Specify order limit for trigger VTWB
 max_order = 12
 # Specify urgent order limit
-max_urgent = 5
+max_urgent = 1
 
 trigger_list = list()
 batching_list = list()
