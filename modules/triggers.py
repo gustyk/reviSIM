051222734799cb3c86474dc21a30e825cdaf7a93
afifdiy <afifdiy@gmail.com>
2022-10-27 17:25:26 +0700

    [New] SimPy based flow

---
+++ modules/triggers.py
@@ -0,0 +1,279 @@
+import pandas as pd
+import time
+from datetime import timedelta
+import copy
+
+class triggers:
+    def __init__(self, env, pickers, batching, routing, cart_capacity):
+        self.env = env
+        self.pickers = pickers
+        self.batching = batching
+        self.routing = routing
+        self.cart_capacity = cart_capacity
+        self.cart_utility = 0
+        self.file_count = 0
+        self.completion_time = timedelta(seconds=0)
+        self.turn_over_time = timedelta(seconds=0)
+        self.total_lateness = timedelta(seconds=0)
+        self.tardy_order = 0
+        self.start_row = 0
+        # start from the second row
+        self.current_row = 1
+        self.total_item = 0
+        self.urgent_status = 0
+        self.current_pool = [[[], 0, []]]
+        self.num_triggered = 0
+        self.processed_item = 0
+        self.batch_orders = list()
+        self.back_order = 0
+        self.all_compl_time = 0
+        self.last_checked_row = 0
+        self.sorted_due_list = list()
+
+    def prepare (self, order_streams):
+        # Start time if need to track timelapse
+        # start_time = time.time()
+        
+        # Assign order file to process
+        self.order_streams = order_streams
+        self.order_streams = order_streams.to_numpy()
+        
+        # Assign simulation limit
+        # self.limit = limit
+        
+        # Assign data from order file to list
+        self.created_time = self.order_streams[:,0]
+        self.total_item = self.order_streams[:,1]
+        self.positions = self.order_streams[:,3]
+        self.total_order = len(self.created_time)
+        
+        # Assign Initial Time
+        self.initial_time = self.created_time[0].replace(minute=0, second=0)
+        self.last_time = self.initial_time
+                
+    def picking_process(self):       
+        # Print process detail if required
+        # print("%d. Start Row: %d, Current Row: %d, Back Order: %d, secs: %f" % (self.num_triggered, self.start_row, self.current_row, self.back_order, time.time() - start_time))
+
+        # If need to go back one order
+        if self.back_order > 0:
+            # Shift order batch taking minus back order count
+            if (self.start_row < self.current_row):
+                batch_orders = self.order_streams[max(0, self.start_row-self.back_order):self.current_row-self.back_order]
+            else:
+                batch_orders = self.order_streams[max(0, self.start_row-self.back_order):self.current_row+1-self.back_order]
+
+            # Update order pool cache
+            item_qty = self.total_item[self.current_row-self.back_order:self.current_row]
+            self.current_pool[0][0] = self.current_pool[0][0][-self.back_order:]
+            self.current_pool[0][1] = sum(item_qty)
+            self.current_pool[0][2] = item_qty.tolist()
+        
+        # No need to go back order, take complete batch
+        # and empty order pool cache
+        else:
+            if (self.start_row < self.current_row):
+                batch_orders = self.order_streams[self.start_row:self.current_row]
+            else:
+                batch_orders = self.order_streams[self.start_row:self.current_row+1]
+            self.current_pool = [[[], 0, []]]
+
+        # Processing batching
+        raw_batch = self.batching.run(batch_orders)
+        ioStation_content = self.batching.collect_batch(raw_batch)
+
+        # Counting cart utility
+        cart_uti = 0
+        f_count = 0
+        for batch in ioStation_content:
+            self.processed_item += batch[1]
+            cart_uti += round(batch[1]/self.cart_capacity, 2)
+            cart_uti = round(cart_uti, 2)
+            f_count += 1
+        self.cart_utility += round(cart_uti/len(ioStation_content), 2)
+        self.file_count += f_count
+
+        # Processing routing variation
+        self.routing.run(ioStation_content)
+        calculated_compl_time = self.routing.count_completion_time()
+        
+        # Counting total completion time
+        for idx, compl_time in enumerate(calculated_compl_time):
+            with self.pickers.request() as req:
+                yield req
+                finSeconds = compl_time.total_seconds() + timedelta(minutes=1).seconds
+                yield self.env.timeout(finSeconds)
+                self.completion_time += compl_time
+                finTime = self.initial_time + timedelta(seconds=self.env.now)
+                for order in raw_batch[idx]:
+                    # order[0] is createdTime
+                    tov_time = (finTime - order[0])
+                    self.turn_over_time += tov_time
+                    # order[2] is dueTime
+                    self.total_lateness += finTime - order[2]
+                    tots = self.total_lateness.total_seconds()
+                    if finTime > order[2]:
+                        self.tardy_order += 1
+
+        # Looping condition
+        self.start_row = self.current_row
+        
+        # Reset back order count
+        self.back_order = 0
+
+    def add_order_to_op(self):
+        if (self.current_row < self.total_order):
+            # Put current order data to order pool
+            self.current_pool[0][0] += self.positions[self.current_row]
+            self.current_pool[0][1] += self.total_item[self.current_row]
+            self.current_pool[0][2].append(self.total_item[self.current_row])
+
+    def ftwb(self):
+        # FTWB
+        delta = timedelta(minutes=12).seconds
+        time_limit = delta
+        while True and self.current_row < self.total_order:
+            self.add_order_to_op()
+            if self.env.now >= time_limit or self.current_row == self.total_order:
+                if self.env.now > time_limit:
+                    self.back_order += 1
+                self.num_triggered += 1
+                self.env.process(self.picking_process())
+                time_limit += delta
+
+            yield self.env.timeout((self.created_time[self.current_row] - self.last_time).seconds)
+
+            self.last_time = self.created_time[self.current_row]
+            # Advance next order data
+            self.current_row += 1
+
+    def vtwb(self):
+        # VTWB
+        order_batch = 12
+        max_order_batch = order_batch
+        while True and self.current_row < self.total_order:
+            self.add_order_to_op()
+            if self.current_row >= max_order_batch or self.current_row == self.total_order:
+                # If current order row exceed order limit go back n row
+                # until fits in order limit
+                if self.current_row > max_order_batch:
+                    self.back_order += 1
+                    orderNum = copy.copy(self.current_row)
+                    while orderNum > max_order_batch:
+                        orderNum -= 1
+                        self.back_order += 1
+                self.num_triggered += 1
+                self.env.process(self.picking_process())
+                max_order_batch += order_batch
+            
+            yield self.env.timeout((self.created_time[self.current_row] - self.last_time).seconds)
+
+            self.last_time = self.created_time[self.current_row]
+            # Advance next order data
+            self.current_row += 1
+            
+    def max_picker(self):
+        # Max Picker
+        while True and self.current_row < self.total_order:
+            self.add_order_to_op()
+            if (self.pickers.count < self.pickers.capacity and self.current_row - self.start_row != 0) or self.current_row == self.total_order:
+                self.num_triggered += 1
+                self.env.process(self.picking_process())
+            
+            yield self.env.timeout((self.created_time[self.current_row] - self.last_time).seconds)
+
+            self.last_time = self.created_time[self.current_row]
+            # Advance next order data
+            self.current_row += 1
+
+    def max_cart(self):
+        # Max Cart
+        while True and self.current_row < self.total_order:
+            self.add_order_to_op()
+            if (self.current_pool[0][1] >= self.cart_capacity and (self.current_row - 1) - self.start_row != 0) or self.current_row == self.total_order:
+                # If current pool cache total qty exceed cart capacity limit go back n row
+                # until fits in order limit
+                if self.current_pool[0][1] > self.cart_capacity:
+                    self.back_order += 1
+                    while sum(self.current_pool[0][2][:-self.back_order]) > self.cart_capacity:
+                        self.back_order += 1
+                self.num_triggered += 1
+                self.env.process(self.picking_process())
+
+            yield self.env.timeout((self.created_time[self.current_row] - self.last_time).seconds)
+
+            self.last_time = self.created_time[self.current_row]
+            # Advance next order data
+            self.current_row += 1
+    
+    def ug_max_picker(self):
+        # Urgent First + Max Picker
+        max_urgent = 1
+        while True and self.current_row < self.total_order:
+            self.add_order_to_op()
+        # check urgent order
+            self.check_urgent()
+            if self.urgent_status >= max_urgent or (self.pickers.count < self.pickers.capacity and self.current_row - self.start_row != 0) or self.current_row == self.total_order:
+                self.num_triggered += 1
+                self.env.process(self.picking_process())
+            
+            yield self.env.timeout((self.created_time[self.current_row] - self.last_time).seconds)
+
+            self.last_time = self.created_time[self.current_row]
+            # Advance next order data
+            self.current_row += 1
+
+    def ug_max_cart(self):
+        # Urgent First + Max Cart
+        max_urgent = 1
+        while True and self.current_row < self.total_order:
+            self.add_order_to_op()
+            # check urgent order
+            self.check_urgent()
+            if self.urgent_status >= max_urgent or (self.current_pool[0][1] >= self.cart_capacity and (self.current_row - 1) - self.start_row != 0) or self.current_row == self.total_order:
+                # If current pool cache total qty exceed cart capacity limit go back n row
+                # until fits in order limit
+                if self.current_pool[0][1] > self.cart_capacity:
+                    self.back_order += 1
+                    while sum(self.current_pool[0][2][:-self.back_order]) > self.cart_capacity:
+                        self.back_order += 1
+                self.num_triggered += 1
+                self.env.process(self.picking_process())
+            
+            yield self.env.timeout((self.created_time[self.current_row] - self.last_time).seconds)
+
+            self.last_time = self.created_time[self.current_row]
+            # Advance next order data
+            self.current_row += 1
+
+    def check_urgent(self):
+        # Check urgent order
+        # Skip if pool cache still empty
+        if self.current_pool[0][1] == 0:
+            self.urgent_status = 0
+            return 0
+
+        # Reset urgent count
+        self.urgent_status = 0
+        if self.last_checked_row != self.current_row:
+            # Calculate completion time based on
+            # current order pool cache
+            to_routing = copy.deepcopy(self.current_pool)
+            self.routing.run(to_routing)
+            compl_time = self.routing.count_completion_time()
+            
+            # Calculate total completion time
+            self.all_compl_time = sum(c.seconds for c in compl_time)
+            
+            # Update last checked row
+            self.last_checked_row = self.current_row
+            
+            # Update sorted due list
+            self.sorted_due_list = sorted(self.order_streams[self.start_row:self.current_row, 2])
+        
+        # Calculate check time and compare with order due
+        check_time = self.initial_time + timedelta(seconds=(self.env.now + self.all_compl_time))
+        idx = 0
+        while idx < len(self.sorted_due_list) and check_time > self.sorted_due_list[idx]:
+            self.urgent_status += 1
+            idx += 1    
---
+++ whOptim.py
@@ -0,0 +1,154 @@
+import time
+import simpy
+import pandas as pd
+import random
+from datetime import timedelta
+from modules import general_function as gf
+
+from modules import pooling_triggers
+from modules import triggers
+from modules import batchings
+from modules import routings
+# Reading order file
+fname = gf.reading_file()
+
+# Processing file
+# Making Index
+a = len(fname) + 1
+order_file = list()
+# Counting total order
+total_order = list()
+# Counting total item picked
+total_item_picked = list()
+
+# Specify order limit for trigger VTWB
+max_order = 12
+# Specify urgent order limit
+max_urgent = 1
+
+trigger_list = list()
+batching_list = list()
+routing_list = list()
+picker_list = list()
+cart_list = list()
+
+total_completion_time = list()
+average_completion_time = list()
+total_turn_over_time = list()
+average_turn_over_time = list()
+average_picker_utility = list()
+average_cart_utility = list()
+total_tardy_order = list()
+total_lateness = list()
+average_lateness = list()
+
+time_limit = timedelta(hours=8).seconds
+
+def order_stream(trigger_opt, batching_opt, routing_opt, picker_number, cart_capacity):
+    fn_idx = 1
+    for fn in fname:    
+        env = simpy.Environment()
+        picker_pool = simpy.Resource(env, capacity=picker_number)
+        batching = batchings.batchings(batching_opt, cart_capacity)
+        routing = routings.routings(routing_opt)
+        trigger = triggers.triggers(env, picker_pool, batching, routing, cart_capacity)
+
+        trigger.prepare(fn)
+
+        if trigger_opt == 1:
+            env.process(trigger.ftwb())
+        elif trigger_opt == 2:
+            env.process(trigger.vtwb())
+        elif trigger_opt == 3:
+            env.process(trigger.max_picker())
+        elif trigger_opt == 4:
+            env.process(trigger.max_cart())
+        elif trigger_opt == 5:
+            env.process(trigger.ug_max_picker())
+        elif trigger_opt == 6:
+            env.process(trigger.ug_max_cart())
+
+        # trigger.run(fn, limit)
+        env.run(until=time_limit)
+
+        # Listing total order
+        total_order.append(trigger.current_row - 1)
+        # Listing total total time
+        total_item_picked.append(sum(trigger.total_item))
+        # Listing total completion time
+        total_completion_time.append(round(trigger.completion_time.total_seconds()/60, 2))
+        # Listing average completion time
+        average_completion_time.append(round(trigger.completion_time.total_seconds()/60/(trigger.current_row - 1), 2))
+        # Listing total Turonver time
+        total_turn_over_time.append(round(trigger.turn_over_time.total_seconds()/60, 2))
+        # Listing avg Turonver time
+        average_turn_over_time.append(round(trigger.turn_over_time.total_seconds()/60/(trigger.current_row - 1), 2))
+        # Counting and listing average picker utility
+        ave_picker_utility = round(trigger.completion_time/(timedelta(hours=8)*picker_number), 2)
+        average_picker_utility.append(ave_picker_utility)
+        # Counting & listing average cart utility
+        ave_cart_utility = round(trigger.cart_utility/trigger.num_triggered, 2)
+        average_cart_utility.append(ave_cart_utility)
+        # Listing total on time delivery
+        total_tardy_order.append(trigger.tardy_order)
+        # Listing Total Lateness
+        total_lateness.append(round(trigger.total_lateness.total_seconds()/60, 2))
+        # Listing Average Lateness
+        average_lateness.append(round(trigger.total_lateness.total_seconds()/(trigger.current_row - 1)/60,2))
+
+        order_file.append(fn_idx)
+        trigger_list.append(trigger_opt)
+        batching_list.append(batching_opt)
+        routing_list.append(routing_opt)
+        picker_list.append(picker_number)
+        cart_list.append(cart_capacity)
+
+        fn_idx += 1
+
+def result_generator():
+    trigger_opts = [1,2,3,4,5,6]
+    batching_opts = [1,2]
+    routing_opts = [1,2]
+    picker_numbers = [1,2,3,4,5,6,7,8,9,10,11,12]
+    cart_opts = [1,2,3]
+
+    for trigger_opt in trigger_opts:
+        for batching_opt in batching_opts:
+            for routing_opt in routing_opts:
+                for picker_number in picker_numbers:
+                    for cart_opt in cart_opts:
+                        cart_capacity = 0
+                        if cart_opt == 1:
+                            cart_capacity = 50
+                        elif cart_opt == 2:
+                            cart_capacity = 100
+                        elif cart_opt == 3:
+                            cart_capacity = 200
+
+                        print('Processing rules %d-%d-%d-%d-%d' % (trigger_opt, batching_opt, routing_opt, picker_number, cart_opt))
+                        order_stream(trigger_opt, batching_opt, routing_opt, picker_number, cart_capacity)
+    
+    # Put result on file
+    result = pd.DataFrame({
+        'OrderFile': order_file,
+        'TriggerMethod': trigger_list,
+        'BatchingMethod': batching_list,
+        'RoutingPolicy': routing_list,
+        'NumOfPickers': picker_list,
+        'CartCapacity': cart_list,
+        'TotalOrder': total_order,
+        'CompletionTime': total_completion_time,
+        'AvgCompletionTime': average_completion_time,
+        'TurnOverTime': total_turn_over_time,
+        'AvgTurnOverTime': average_turn_over_time,
+        'TotalItemPicked': total_item_picked,
+        'AvgPickerUtil': average_picker_utility,
+        'AvgCartUtil': average_cart_utility,
+        'NumOfLate': total_tardy_order,
+        'TotalLateness': total_lateness,
+        'AvgLateness': average_lateness
+        }, columns = ['OrderFile','TriggerMethod','BatchingMethod','RoutingPolicy','NumOfPickers','CartCapacity','TotalOrder','CompletionTime','AvgCompletionTime','TurnOverTime', 'AvgTurnOverTime','TotalItemPicked','AvgPickerUtil','AvgCartUtil','NumOfLate','TotalLateness','AvgLateness'])
+    result.to_csv('result/All.csv')
+    print('All.csv')
+
+result_generator()
