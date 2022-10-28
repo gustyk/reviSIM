837fbd0ad1444b33931195045b082b9a0e4f97af
afifdiy <afifdiy@gmail.com>
2022-10-28 07:25:26 +0700

    seed batching based on due

--- modules/batchings.py
+++ modules/batchings.py
@@ -1,4 +1,6 @@
 from modules import general_function as gf
+import numpy as np
+
 class batchings:
     def __init__(self, opt, cart_capacity):
         self.opt = opt
@@ -9,7 +11,7 @@ class batchings:
         if self.opt == 1:
             return self.fcfs()
         elif self.opt == 2:
-            return self.seed()
+            return self.seed_due()
 
     def fcfs(self):
         # fcfs
@@ -59,6 +61,97 @@ class batchings:
                     if len(ob) > len(aisle_position[in_start]):
                         ob = aisle_position[in_start]
                         ins = in_start
+                total += qt[ins]
+                index_start.remove(ins)
+                in_ob.append(ins)
+            # Checking possible OPY
+            if len(index_start) == 0:
+                index_list.append(in_ob)
+            else:
+                ob_list = list()
+                for ind in index_start:
+                    if (total + qt[ind]) <= self.cart_capacity:
+                        ob_list.append(ind)
+                # If there's no possible OPY
+                if len(ob_list) == 0:
+                    index_list.append(in_ob)
+                    total = 0
+                    in_ob = list()
+                    ob = list()
+                # If there's possible OPY
+                else:
+                    if len(ob_list) == 1:
+                        in_samad = ob_list[0]
+                    else: 
+                        sad_tb = list()
+                        for obl in ob_list:
+                            sad = list()
+                            for r in aisle_position[obl]:
+                                tb = list()
+                                for b in ob:
+                                    tb.append(abs(r-b))
+                                tb.sort()
+                                sad.append(tb[0])
+                            sad_tb.append(sad)
+                        sad_tr = list()
+                        for obl in ob_list:
+                            sad = list()
+                            for b in ob:
+                                tr = list()
+                                for r in aisle_position[obl]:
+                                    tr.append(abs(b-r))
+                                tr.sort()
+                                sad.append(tr[0])
+                            sad_tr.append(sad)
+                        amad = list()
+                        i = 0
+                        while i < len(sad_tb):
+                            anad_rb = sum(sad_tb[i])/len(sad_tb[i])
+                            anad_br = sum(sad_tr[i])/len(sad_tr[i])
+                            amad.append((anad_rb + anad_br) / 2)
+                            i += 1
+                        samad = amad[0]
+                        in_samad = ob_list[0]
+                        i = 1
+                        while i < len(amad):
+                            if samad > amad[i]:
+                                samad = amad[i]
+                                in_samad = ob_list[i]
+                            i += 1
+                    in_ob.append(in_samad)
+                    total += qt[in_samad]
+                    ob += aisle_position[in_samad]
+                    index_start.remove(in_samad)
+                    if len(index_start) == 0:
+                        index_list.append(in_ob)
+        rfile = list()
+        for il in index_list:
+            fl = self.file[il]
+            rfile.append(fl)
+        return rfile
+    
+    def seed_due(self):
+        # seed
+        # sort by due ascending
+        self.file = self.file[self.file[:, 2].argsort()]
+        qt = self.file[:,1]
+        position = self.file[:,3]
+        aisle_position = list()
+        for post in position:
+            aisle = list()
+            for pos in post:
+                aisle.append(pos[0])
+            aisle_position.append(aisle)
+        total = 0
+        index_start = list(range(0, len(qt)))
+        index_list = list()
+        in_ob = list()
+        ob = list()
+        while len(index_start) != 0:
+            # Setting OB if OB is empty
+            if len(ob) == 0:
+                ob = aisle_position[index_start[0]]
+                ins = index_start[0]
                 total += qt[ins]
                 index_start.remove(ins)
                 in_ob.append(ins)
